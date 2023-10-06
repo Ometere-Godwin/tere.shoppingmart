424b033b2d52011860a6f0860668a20948120593
Ometere <ookuoluse@yahoo.com>
2023-10-06 18:49:40 +0100

    redux-persist, noop dummy storage

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { Provider } from 'react-redux';
 import {SessionProvider} from "next-auth/react";
-import { store } from '@/redux/store';
+import { persistor, store } from '@/redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function Layout({children} : {children: React.ReactNode}) {
   return (
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
       <SessionProvider>{children}</SessionProvider>
+      </PersistGate>
     </Provider>
   )
 }
